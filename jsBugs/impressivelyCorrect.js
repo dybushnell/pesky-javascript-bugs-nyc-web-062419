c31222b408dac87213bbbacaae2e64ea9c361d4a
Dave Bushnell <dybushnell@gmail.com>
2019-08-12 10:13:43 -0400

    finished part 1

--- jsBugs/impressivelyCorrect.js
+++ jsBugs/impressivelyCorrect.js
@@ -1,19 +1,42 @@
-const students = [
-  { name: 'Bobby', hometown: 'Pittsburgh', labsComplete: 801 },
-  { name: 'Sally', hometown: 'New York', labsComplete: 398 },
-  { name: 'Sammy', hometown: 'Pittsburgh', labsComplete: 550 },
-  { name: 'Annette', hometown: 'Los Angeles', labsComplete: 896 },
-  { name: 'Joey', hometown: 'Tampa Bay', labsComplete: 0 }
+const students = [{
+    name: 'Bobby',
+    hometown: 'Pittsburgh',
+    labsComplete: 801
+  },
+  {
+    name: 'Sally',
+    hometown: 'New York',
+    labsComplete: 398
+  },
+  {
+    name: 'Sammy',
+    hometown: 'Pittsburgh',
+    labsComplete: 550
+  },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles',
+    labsComplete: 896
+  },
+  {
+    name: 'Joey',
+    hometown: 'Tampa Bay',
+    labsComplete: 0
+  }
 ]
 
 const totalLabsComplete = students => {
   return students.reduce((total, student) => {
-    return labsComplete + total
+    total = totalLabsComplete + student
   })
 }
 
 const averageLabsComplete = students => {
-  return totalLabsComplete(students) / students.length
+  let total = 0
+  students.forEach(student => {
+    total = total + student.labsComplete
+  });
+  return (total / students.length)
 }
 
 console.log(averageLabsComplete(students))
